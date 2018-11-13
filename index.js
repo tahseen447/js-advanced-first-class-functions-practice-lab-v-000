4e499780c94e0d45b881d6622a49ea4ee71c1987
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-13 00:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logDriverNames = function(drivers){
   drivers.forEach(function(item){
     console.log(item.name);
   });
-};
+}
 const logDriversByHometown = function(drivers, matcher) {
   drivers.forEach(function(item){
     if(item.hometown === matcher){
